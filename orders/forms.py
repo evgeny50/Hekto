0e439e6b1cdd796775bf20f21cf792d6b4bc0daa
evgeny3748@gmail.com <evgeny3748@gmail.com>
2022-02-09 19:23:43 +0500

    Added template order page

--- orders/forms.py
+++ orders/forms.py
@@ -4,7 +4,24 @@ from .models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
+
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email',
                   'address', 'postal_code', 'city']
+
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'order-form-email',
+                                             'placeholder': 'Email or mobile phone number'}),
+            'first_name': forms.TextInput(attrs={'class': 'order-form-name',
+                                                 'placeholder': 'First name (optional)'}),
+            'last_name': forms.TextInput(attrs={'class': 'order-form-name',
+                                                 'placeholder': 'First name (optional)'}),
+            'address': forms.TextInput(attrs={'class': 'order-form-email address',
+                                                 'placeholder': 'Address'}),
+            'city': forms.TextInput(attrs={'class': 'order-form-name',
+                                                'placeholder': 'City'}),
+            'postal_code': forms.TextInput(attrs={'class': 'order-form-name city',
+                                                'placeholder': 'Postal Code'}),
+
+        }
